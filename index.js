08e798c48e1f8d2870b4b46d2ced7d4bbc709099
zabedhasan129 <hasanalzabed2013@gmail.com>
2025-09-04 15:11:48 +0600

    heart_icon_Counter

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("never give up")
+//heart icon counter
+let count = 0
+let heartbuttons = document.querySelectorAll(".common_heart")
+
+let counter = document.getElementById("heartCounter")
+
+for (let button of heartbuttons) {
+    button.addEventListener("click", function () {
+        count++;
+        counter.innerText = count
+    })
+}
